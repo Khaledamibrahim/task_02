caf2fa47f5b2513e47dbc352feb64dd1b71e06a0
Khaled Ibrahim <khaledamibrahim@gmail.com>
2018-07-02 20:25:02 +0300

    task 2 project with shehab

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def view_renderer(request):
+	context = {
+	"msg": "Hello World"
+	}
+	return render(request, "homepage.html", context)
